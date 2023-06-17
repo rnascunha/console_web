5033df426382848ce616fe330421fbd161bf21b3
Rafael Cunha <rnascunha@gmail.com>
2023-06-16 21:02:18 -0300

    Creating binary input (incomplete)

--- src/tools/input/input.ts
+++ src/tools/input/input.ts
@@ -1,12 +1,20 @@
 import './input.css';
 import '../../ts/components/binary-dump/binary-dump';
+import '../../ts/components/binary-input/text-binary';
 import { BinaryDump } from '../../ts/components/binary-dump/binary-dump';
 
 const input = document.querySelector('#input') as HTMLInputElement;
 const bl = document.querySelector('#breakline') as HTMLInputElement;
 const bd = document.querySelector('#data-dump') as BinaryDump;
 const encoder = new TextEncoder();
+const input_binary = document.querySelector('#input-binary') as BinaryInput;
 
 bl.onchange = () => bd.update(encoder.encode(input.value), +bl.value);
 input.onkeyup = () => bd.update(encoder.encode(input.value), +bl.value);
 input.dispatchEvent(new Event('keyup'));
+document.querySelectorAll('input[name=encode]').forEach(v => {
+  const i = v as HTMLInputElement;
+  i.onclick = ev => {
+    input_binary.encode = i.value as Encoding;
+  };
+});
